d6e6ec05bab08246ff6a40eae7af14356f177b6e
wabbajack1 <kidim4444@gmail.com>
2024-01-23 10:38:34 +0100

    faiss databse

--- chain_logic.py
+++ chain_logic.py
@@ -1,52 +1,52 @@
-import langchain
+from document_store import get_chroma
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.llms import Ollama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
-from langchain_openai import OpenAI
-
-
-from document_store import get_document_store
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from template_prompt import Legal_Template
+import langchain
 
-langchain.verbose = True
+
+
+
+# langchain.verbose = True
 langchain.debug = True
 
-
-def get_model(base_url: str, model_name: str = "phi"):
-    if model_name == "open-ai":
-        return OpenAI()
-
+def get_model(base_url:str, model_name:str="mistral"):
     ollama_embeddings = OllamaEmbeddings(base_url=base_url, model=model_name)
     ollama = Ollama(base_url=base_url, model=model_name)
     return ollama
 
-
 if __name__ == "__main__":
-    model = get_model("http://localhost:11434", model_name="open-ai")
 
-    # choose task
+    model = get_model("http://127.0.0.1:11434")
+    
+    # choos task
     tasks_retriever = Legal_Template()
     task_template = tasks_retriever.task1_template()
 
-    template = """Answer the question based only on the following context. If you do not
-        know the answer or want to generate your answer based on your model, respond by saying "I do not know.":
+    template = """Answer the question based only on the following context:
         {context}
 
         Question: {question}
+
+        If you know the answer, give it in bullet points. If you do not know the answer, respond by saying "I do not know."
         """
-
-    retriever = get_document_store(model_name="open-ai").as_retriever()
-
+    
     # init task
     prompt = ChatPromptTemplate.from_template(template)
     chain = (
-            {"context": retriever, "question": RunnablePassthrough()}
-            | prompt
-            | model
-            | StrOutputParser()
+        {"context": get_chroma().as_retriever(), "question": RunnablePassthrough()}
+        | prompt
+        | model
+        | StrOutputParser()
     )
 
-    # print(chain.invoke("How can companies contribute substantially to climate change mitigation?"))
-    print(chain.invoke("What are environmental objectives provided by the EU taxonomy?"))
+    """
+    An economic activity that pursues the environmental objective of climate change mitigation should contribute substantially to 
+    the stabilisation of greenhouse gas emissions by avoiding or reducing them or by enhancing greenhouse gas removals. The economic activity 
+    should be consistent with the long-term temperature goal of the Paris Agreement. That environmental objective should be interpreted in accordance 
+    with relevant Union law, including Directive 2009/31/EC of the European Parliament and of the Council.
+    """
+    print(chain.invoke("How should an economic activity pursue its objective?"))
